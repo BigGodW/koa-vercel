ee32a2244b65b627f593efe26be331e6e81c2678
wuyong <biggodw@gmail.com>
2023-12-13 23:16:25 +0800

    router-bug1

--- router/index.js
+++ router/index.js
@@ -2,7 +2,7 @@ import Router from 'koa-router'
 
 const router = new Router()
 router.get('/',ctx=>{
-    ctx.body = 'hello koa-router'
+    ctx.body = 'hello koa-router-路由bug'
 })
 router.get('/user',ctx=>{
     ctx.body = 'user'
