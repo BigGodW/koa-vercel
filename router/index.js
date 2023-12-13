1d62ba1ff880042062528383dab617df86c0123c
wuyong <biggodw@gmail.com>
2023-12-14 01:10:12 +0800

    add sqlite

--- router/index.js
+++ router/index.js
@@ -8,6 +8,18 @@ router.get('/user',async ctx=>{
     ctx.body = await ctx.prisma.user.findMany()
     
 })
+router.post('/user',async ctx=>{
+    const {name,email} = ctx.request.body
+    const res = await ctx.prisma.user.create({
+        data: {
+          name,
+          email
+        },
+      })
+      console.log(res)
+    ctx.body = {message:'添加用户成功',res}
+
+})
 router.post('/',ctx=>{
     const data = ctx.request.body
     ctx.body = data
