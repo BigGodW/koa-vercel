03231ce6202c9dcd4e5982add9f88a5d232b3370
wuyong <biggodw@gmail.com>
2023-12-13 23:23:21 +0800

    public

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-import Koa from 'koa'
-import router from './router/index.js'
-const app = new Koa()
-app.use(router.routes(),router.allowedMethods())
+import Koa from "koa";
+import router from "./router/index.js";
+import koaStatic from "koa-static";
+const app = new Koa();
+// 静态资源配置
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use(koaStatic(__dirname + "/public"));
 
+// 挂载路由
+app.use(router.routes(), router.allowedMethods());
 
 app.listen(3008, () => {
-    console.log('3008项目启动')
+  console.log("3008项目启动");
 });
